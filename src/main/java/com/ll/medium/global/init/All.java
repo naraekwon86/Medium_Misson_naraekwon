1b4529009e3a1ff4ddc95a73ac9a296ebd6b9e09
naraekwon86 <wde0530@gmail.com>
2023-12-14 15:25:35 +0900

    lesson 20

--- src/main/java/com/ll/medium/global/init/All.java
+++ src/main/java/com/ll/medium/global/init/All.java
@@ -1,29 +1,30 @@
 package com.ll.medium.global.init;
 
+import com.ll.medium.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.ll.medium.domain.member.member.service.MemberService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
+
 @Configuration
-@Profile("!prod")
 @Slf4j
 @RequiredArgsConstructor
-public class NotProd {
+public class All {
     private final MemberService memberService;
 
     @Bean
-    @Order(3)
-    public ApplicationRunner initNotProd(){
+    @Order(2)
+    public ApplicationRunner initAll(){
         return args -> {
-            if (memberService.findByUsername("user1").isPresent()) return;
+            if (memberService.findByUsername("system").isPresent()) return;
 
-            memberService.join("user1", "1234");
-            memberService.join("user2", "1234");
+            memberService.join("system", "1234");
+            memberService.join("admin", "1234");
+
+
         };
     }
 }
