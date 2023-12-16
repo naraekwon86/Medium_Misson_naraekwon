009c5d45e6ab007c1015dca0f5334d7cc2891e25
naraekwon86 <wde0530@gmail.com>
2023-12-16 15:16:25 +0900

    lesson 26

--- src/main/java/com/ll/medium/global/init/NotProd.java
+++ src/main/java/com/ll/medium/global/init/NotProd.java
@@ -8,6 +8,8 @@ import com.ll.medium.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.post.post.service.PostService;
 
 @Configuration
 @Profile("!prod")
@@ -15,15 +17,26 @@ import org.springframework.core.annotation.Order;
 @RequiredArgsConstructor
 public class NotProd {
     private final MemberService memberService;
+    private final PostService postService;
 
     @Bean
     @Order(3)
     public ApplicationRunner initNotProd(){
         return args -> {
             if (memberService.findByUsername("user1").isPresent()) return;
+            Member memberUser1 = memberService.join("user1", "1234").getData();
+            Member memberUser2 = memberService.join("user2", "1234").getData();
+            Member memberUser3 = memberService.join("user3", "1234").getData();
 
-            memberService.join("user1", "1234");
-            memberService.join("user2", "1234");
+            postService.write(memberUser1, "제목 1", "내용 1", true);
+            postService.write(memberUser1, "제목 2", "내용 2", true);
+            postService.write(memberUser1, "제목 3", "내용 3", false);
+            postService.write(memberUser1, "제목 4", "내용 4", true);
+
+            postService.write(memberUser2, "제목 5", "내용 5", true);
+            postService.write(memberUser2, "제목 6", "내용 6", false);
+
+
         };
     }
 }
